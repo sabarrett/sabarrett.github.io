c7a6144fd58ad0dd2e691b1d254e60005d5c3fe8
sabarrett <scottbarrett99@gmail.com>
2024-06-02 10:48:40 -0400

    Add index and go networked code post

--- publish-script.el
+++ publish-script.el
@@ -1,7 +1,30 @@
+(package-initialize)
+
+(require 'ox-publish)
+(require 'font-lock)
+(require 'htmlize)
 (require 'org)
 
-(org-publish
-           '("website"
-              :base-directory "."
-              :publishing-function org-html-publish-to-html
-              :publishing-directory "./public_html"))
+(setq org-publish-alist
+      '(("website"
+         :base-directory "./src"
+	 :base-extension any
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "./public_html"
+	 :html-head "<link rel=\"stylesheet\" href=\"https://latex.now.sh/style.css\">"
+	 :html-validation-link nil
+	 :section-numbers nil
+	 :with-sub-superscript nil
+	 :with-toc t
+	 :with-creator nil
+	 :with-author nil
+	 :headline-levels 10)
+	("images"
+	 :base-directory "./img"
+	 :base-extension "png"
+	 :publishing-function org-publish-attachment
+	 :publishing-directory "./public_html")))
+
+(org-publish-all t)
+
+(message "Build complete!")
