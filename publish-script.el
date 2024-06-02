77a314a63058a2ddb685b50e86d4b63e83edb3b0
sabarrett <scottbarrett99@gmail.com>
2024-06-02 10:57:06 -0400

    Add melpa to build script

--- publish-script.el
+++ publish-script.el
@@ -1,17 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+(package-install 'htmlize)
 
 (require 'ox-publish)
 (require 'font-lock)
 (require 'htmlize)
 (require 'org)
 
-(setq org-publish-alist
+(setq org-publish-project-alist
       '(("website"
-         :base-directory "./src"
+         :base-directory "./src/"
 	 :base-extension any
          :publishing-function org-html-publish-to-html
          :publishing-directory "./public_html"
-	 :html-head "<link rel=\"stylesheet\" href=\"https://latex.now.sh/style.css\">"
 	 :html-validation-link nil
 	 :section-numbers nil
 	 :with-sub-superscript nil
@@ -20,7 +23,7 @@
 	 :with-author nil
 	 :headline-levels 10)
 	("images"
-	 :base-directory "./img"
+	 :base-directory "./img/"
 	 :base-extension "png"
 	 :publishing-function org-publish-attachment
 	 :publishing-directory "./public_html")))
